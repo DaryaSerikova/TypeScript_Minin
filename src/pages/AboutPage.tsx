37cd94ce4f19b29113a1191b76184608a550cbdd
Darya Serikova <dvserikova@gmail.com>
2022-09-27 12:11:10 +0700

    all project

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export function AboutPage() {
+  return(
+    <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam quos fugit modi praesentium, earum voluptatem amet incidunt voluptate nam voluptates dignissimos neque adipisci harum est tempora nostrum repellendus culpa nesciunt unde cupiditate dolore qui? Exercitationem doloribus in dignissimos possimus dolor rem magni esse, cumque facilis accusantium perspiciatis reiciendis suscipit quisquam.</div>
+  )
+}
+
+//lorem50 Enter
